94041a84fa2cf667758266f976ba9ccf89278417
nashare <tatiana.lavr@gmail.com>
2023-11-06 23:08:17 -0800

    delete link import

--- learn-spanish/src/pages/TestCompletePage/TestCompletePage.tsx
+++ learn-spanish/src/pages/TestCompletePage/TestCompletePage.tsx
@@ -1,6 +1,4 @@
 import "./TestCompletePage.css";
-import { Link } from "react-router-dom";
-
 
 export const TestCompletePage = () => {
     return <><p>Complete</p></>
--- learn-spanish/src/pages/TestPage/TestPage.tsx
+++ learn-spanish/src/pages/TestPage/TestPage.tsx
@@ -1,5 +1,4 @@
 import "./TestPage.css";
-import { Link } from "react-router-dom";
 
 
 export const TestPage = () => {
